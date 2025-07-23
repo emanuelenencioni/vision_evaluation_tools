a14ea94149b22690949f45406a43e09b9ccdaadf
Emanuele Nencioni <emanuele.nencioni1@gmail.com>
2025-07-23 12:05:01 +0200

    added argparsing, stride

--- Extract images from bags/ros2_image_extraction.py
+++ Extract images from bags/ros2_image_extraction.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     
     camera_model = config['camera_model']
     focal_length = config['focal_length']
+    stride = int(config['frame_interval'])
 
     exif_bytes_left = piexif.dump({"0th": {piexif.ImageIFD.Make: u"StereoLab", piexif.ImageIFD.Model: u"Zed2i"}, 
         "Exif": {piexif.ExifIFD.FocalLength: (int(focal_length * 1000),1000), piexif.ExifIFD.BodySerialNumber: "8007232"}})
@@ -31,11 +32,17 @@ if __name__ == "__main__":
         # topic and msgtype information is available on .connections list
         for connection in reader.connections:
             print(connection.topic, connection.msgtype)
-        for connection, timestamp, rawdata in tqdm(reader.messages()):
+        i = 0
+        for connection, timestamp, rawdata in tqdm(reader.messages(), total=reader.message_count, desc="Extracting images"):
             if connection.topic == '/zed/zed_node/left_raw/image_raw_color': # topic Name of images
-                img_path =save_path + "frame%04i.jpg" % count
-                msg = reader.deserialize(rawdata, connection.msgtype)
-                img = message_to_cvimage(msg, 'bgr8') # change encoding type if needed
-                cv2.imwrite(img_path, img)
-                piexif.insert(exif_bytes_left, img_path)
-                count += 1
+                if i % stride != 0: 
+                    i+= 1
+                    continue
+                else:
+                    img_path =save_path + "frame%04i.jpg" % count
+                    msg = reader.deserialize(rawdata, connection.msgtype)
+                    img = message_to_cvimage(msg, 'bgr8') # change encoding type if needed
+                    cv2.imwrite(img_path, img)
+                    piexif.insert(exif_bytes_left, img_path)
+                    count += 1
+                    i+= 1
